30b941f11fce57a898c0d47845a46c54a35e42dd
MateusPereiradev <dev.mateuspereira@gmail.com>
2024-11-18 20:58:16 -0300

    testes

---
+++ script.js
@@ -0,0 +1,18 @@
+const nome = "javascript";
+
+console.log(nome);
+
+const livro = {
+  nome: "O senhor dos anéis",
+  ano: 2001,
+  autor: "J.R.R. tolkien",
+};
+
+const filme = {
+  nome: "O senhor dos anéis",
+  ano: 2001,
+  autor: "J.R.R. tolkien",
+  diretor: "Peter Jackson",
+};
+
+console.log(filme.ano + " " + filme.autor);
