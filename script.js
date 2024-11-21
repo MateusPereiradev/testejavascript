d9cb81ad2d9efe1d5cc18cfd06c55bebfb7a6e18
MateusPereiradev <dev.mateuspereira@gmail.com>
2024-11-21 18:05:48 -0300

    treinando funções, objetos e return

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-const nome = "javascript";
+function livro(nome, ano, autor) {
+  const nomeMaior = nome.toUpperCase();
+  const anoLivro = 2050 - ano;
+  const frase = nome.toUpperCase() + " por " + autor;
+  const objeto = {
+    nome: nomeMaior,
+    anoLivro,
+    frase,
+  };
+  return objeto;
+}
 
-console.log(nome);
+const livroRetorno = livro("O Senhor dos Anéis", 1954, "J.R.R.Tolkien");
 
-const livro = {
-  nome: "O senhor dos anéis",
-  ano: 2001,
-  autor: "J.R.R. tolkien",
-};
-
-const filme = {
-  nome: "O senhor dos anéis",
-  ano: 2001,
-  autor: "J.R.R. tolkien",
-  diretor: "Peter Jackson",
-};
-
-console.log(filme.ano + " " + filme.autor);
+console.log(livroRetorno.frase);
